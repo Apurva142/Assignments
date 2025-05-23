cee79d69dae95f7df26c5853ba130977f05ccaa3
Apurva <apurvaraj.p@gmail.com>
2025-05-23 13:54:53 +0530

    Assignment 1 completed related to DataTypes

---
+++ src/assignment/Assignment1_DataType.java
@@ -0,0 +1,100 @@
+package assignment;
+
+public class Assignment1_DataType {
+
+	public static void main(String[] args) {
+
+		//Below is the format to represent a float DataType
+		float Temperature = 25.5f;
+		System.out.println("Temperature of a city in degrees Celsius: " + Temperature);
+
+		//Below is the format to represent a boolean DataType
+		boolean OrderPlaced = false;
+		System.out.println("Whether a customer has placed an order: " + OrderPlaced);
+
+		//Below is the format to represent a String
+		String PhoneNumber = "123-456-7890";
+		System.out.println("Person's phone number: " + PhoneNumber);
+
+		//Below is the format to represent a float DataType
+		float BalanceAmount = 1000.50f;
+		System.out.println("Amount of money in a customer's bank account: " + BalanceAmount);
+
+		//Below is the format to represent a String
+		String Email = "john.doe@example.com";
+		System.out.println("Person's email address: " + Email);
+
+		//Below is the format to represent a float DataType
+		float latitude = 37.7749f;
+		float longitude = -122.4194f;
+		System.out.println("Coordinates of a location (latitude, longitude): " + latitude+ ","+longitude);
+
+		//Below is the format to represent a boolean DataType
+		boolean MaritalStatus = true;
+		System.out.println("Person's marital status: " + MaritalStatus);
+
+		//Below is the format to represent a String
+		String Occupation = "Software Engineer";
+		System.out.println("Person's occupation: " + Occupation);
+
+		String FavouriteColor = "Blue";
+		System.out.println("Person's favourite colour: " + FavouriteColor);
+
+		//Below is the format to represent a short DataType
+		short CurrentYear = 2023;
+		System.out.println("Current year: " + CurrentYear);
+
+		//Below is the format to represent a String
+		String Followers = "1,000,000";
+		System.out.println("Number of followers on a social media platform: " + Followers);
+
+		//Below is the format to represent a float DataType
+		float Rating = 7.5f;
+		System.out.println("Rating of a movie: " + Rating);
+
+		//Below is the format to represent a char DataType
+		char BloodType = 'A';
+		System.out.println("Person's blood type: " + BloodType);
+
+		//Below is the format to represent a String
+		String BookTitle = "To Kill a Mockingbird";
+		System.out.println("Title of a book: " + BookTitle);
+
+		//Number of employees in a company: 500
+		short TotalNumberOfEmployees = 500;
+		System.out.println("Number of employees in a company: " + TotalNumberOfEmployees);
+
+		//Below is the format to represent a String
+		String EventTime = "2:30 PM";
+		System.out.println("Time of an event: " + EventTime);
+
+		String CountryName = "United States";
+		System.out.println("Name of a country: " + CountryName);
+
+		String EyeColor = "Brown";
+		System.out.println("Person's eye color: " + EyeColor);
+
+		String Birthplace = "New York City";
+		System.out.println("Person's birthplace: " + Birthplace);
+
+		//Below is the format to represent a float DataType
+		float TwoCitiesDistance = 200.5f;
+		System.out.println("Distance between two cities: " + TwoCitiesDistance);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
