f7287184c93b4e830e6a2a923bfc12593d219175
Apurva <apurvaraj.p@gmail.com>
2025-05-27 16:41:35 +0530

    Assignment 3 completed

---
+++ src/assignment/EmpGroups.java
@@ -0,0 +1,23 @@
+package assignment;
+
+public class EmpGroups {
+
+	public static void main(String[] args) {
+		Employees obj = new Employees();
+		String[] EmployeeNames = new String[3];
+		EmployeeNames[0]=obj.EmployeeName1;
+		EmployeeNames[1]=obj.EmployeeName2;
+		EmployeeNames[2]=obj.EmployeeName3;
+		
+		String[] EmployeeIDs= new String[3];
+		EmployeeIDs[0] = obj.EmployeeID1;
+		EmployeeIDs[1] = obj.EmployeeID2;
+		EmployeeIDs[2] = obj.EmployeeID3;
+		
+		System.out.println("Employee Name:"+EmployeeNames[0]+","+"Employee ID:"+EmployeeIDs[0]);
+		
+		System.out.println("Employee Name:"+EmployeeNames[1]+","+"Employee ID:"+EmployeeIDs[1]);
+		System.out.println("Employee Name:"+EmployeeNames[2]+","+"Employee ID:"+EmployeeIDs[2]);
+	}
+
+}
---
+++ src/assignment/Employees.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class Employees {
+	
+	String EmployeeName1 = "Bharath";
+	String EmployeeName2 = "ABC";
+	String EmployeeName3 = "XYZ";
+	
+	String EmployeeID1 = "1234";
+	String EmployeeID2 = "1235";
+	String EmployeeID3 = "1236";
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
